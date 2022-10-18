39053fe1b9ca5d9f3855f8de6d2635b38e144c2c
Monika Piesyk (External) <mpiesyk@blackberry.com>
2022-10-18 16:14:14 +0200

    Initial Commit

---
+++ fewThreads/fewThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  fewThreadsApp.swift
+//  fewThreads
+//
+//  Created by Monika Piesyk on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct fewThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
